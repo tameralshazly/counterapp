d29d5dff7db7aca277c5598416b7f78242a206a1
tameralshazly <tameralshazly@yahoo.com>
2023-07-14 22:59:48 +0400

    Step-00

---
+++ lib/presentation/screens/home_screen/widgets/home_widget.dart
@@ -0,0 +1,12 @@
+// Dummy Widget
+
+import 'package:flutter/material.dart';
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
